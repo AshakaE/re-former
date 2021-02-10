cd78f37f4d7ed4a6aac16a6bf5971a17824df5e8
AshakaE <ashakaegerega@gmail.com>
2021-02-10 23:46:44 +0100

    Extra credit, readme and rubocop linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save 
+    if @user.save
       redirect_to new_user_path
     else
       render :new
@@ -19,14 +19,16 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to edit_user_path
+      redirect_to user_path(@user)
     else
-      render :edit
+      flash[:errors] = @user.errors.full_messages
+      redirect_to edit_user_path
     end
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:username,:email,:password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
